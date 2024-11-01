c7aa97c3b26529b5bd37426a8d79baaed001a55e
avg77 <guptaavish03@gmail.com>
2024-11-01 20:12:29 +0530

    update HeroSection.jsx

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -14,8 +14,8 @@ const HeroSection = () => {
         <div className={Style.heroSection_box_left}>
           <h1>Discover, collect, and sell NFTs 🖼️</h1>
           <p>
-            Discover the most outstanding NTFs in all topics of life. Creative
-            your NTFs and sell them
+            Discover the most outstanding NTFs in all topics of life. Create
+            your own NTFs and sell them.
           </p>
           <Button btnName="Start your search" handleClick={() => router.push("/searchPage")}/>
         </div>
